3ec742cdab9b1aaa5d86e11fee589afb4fab08e3
Aleksey Dolgii <dolgii@devexperts.com>
2015-09-28 19:32:07 +0300

    Project

--- src/am_i_an_otter/core.clj
+++ src/am_i_an_otter/core.clj
@@ -1,6 +1,18 @@
-(ns am-i-an-otter.core)
+(ns am-i-an-otter.core
+  (:use compojure.core)
+  (:require [compojure.route :as route]
+            [compojure.handler :as handler]
+            [ring.middleware.multipart-params :as mp]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(load "imports")
+(load "otters-db")
+(load "otters")
+
+(defroutes main-routes
+           (GET "/" [] (page-compare-otters))
+           (GET ["/upvote/:id" :id #"[0-9]+"] [id] (page-upvote-otter id))
+           (GET "/upload" [] (page-start-upload-otter))
+           (GET "/votes" [] (page-otter-votes))
+
+
+           )
