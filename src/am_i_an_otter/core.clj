a14a6cf192bcdd3f6edd1eea142fd742c3b06925
Aleksey Dolgii <dolgii@devexperts.com>
2015-09-28 18:34:39 +0300

    Initial commit

---
+++ src/am_i_an_otter/core.clj
@@ -0,0 +1,6 @@
+(ns am-i-an-otter.core)
+
+(defn foo
+  "I don't do a whole lot."
+  [x]
+  (println x "Hello, World!"))
---
+++ test/am_i_an_otter/core_test.clj
@@ -0,0 +1,7 @@
+(ns am-i-an-otter.core-test
+  (:require [clojure.test :refer :all]
+            [am-i-an-otter.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
